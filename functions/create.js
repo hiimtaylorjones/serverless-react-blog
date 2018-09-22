aa720e1b9a585326694f697d7de5e80ba995c7c6
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-09-22 19:38:58 -0400

    setting up serverless config

---
+++ functions/create.js
@@ -0,0 +1,23 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "posts",
+    Item: {
+      postId: uuid.v1(),
+      title: data.title,
+      body: data.body,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
