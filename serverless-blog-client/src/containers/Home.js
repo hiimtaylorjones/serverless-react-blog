ffe56b53ac8346857d4f28bfab6c3f42864cb9ef
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-09-27 08:37:59 -0400

    Post Index API calls

--- serverless-blog-client/src/containers/Home.js
+++ serverless-blog-client/src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { PageHeader, ListGroup } from "react-bootstrap";
+import { API } from "aws-amplify";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 
 export default class Home extends Component {
@@ -12,8 +13,48 @@ export default class Home extends Component {
     };
   }
 
-  renderPostsList(notes) {
-    return null;
+  async componentDidMount() {
+    try {
+      const posts = await this.posts();
+      this.setState({ posts });
+    } catch (e) {
+      alert(e);
+    }
+  
+    this.setState({ isLoading: false });
+  }
+
+  posts() {
+    return API.get("posts", "/posts");
+  }
+
+  renderPostsList(posts) {
+    return [{}].concat(posts).map(
+      (post, i) =>
+        i !== 0
+          ? <ListGroupItem
+              key={posts.postId}
+              href={`/notes/${post.postId}`}
+              onClick={this.handlePostClick}
+              header={post.title.trim().split("\n")[0]}
+            >
+              {"Created: " + new Date(post.createdAt).toLocaleString()}
+            </ListGroupItem>
+          : <ListGroupItem
+              key="new"
+              href="/posts/new"
+              onClick={this.handlePostClick}
+            >
+              <h4>
+                <b>{"\uFF0B"}</b> Create a new post
+              </h4>
+            </ListGroupItem>
+    );
+  }
+
+  handlePostClick = event => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute("href"));
   }
 
   renderPosts() {
