e5b670c6c8e1f636a54bafd95b3bd9d036dbf9ac
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-10-02 22:18:06 -0400

    Getting api to work properly

--- serverless-blog-client/src/index.js
+++ serverless-blog-client/src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Amplify from "aws-amplify";
+
+import config from "./config";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
+
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: "serverless-blog-api",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 ReactDOM.render(
   <Router>
